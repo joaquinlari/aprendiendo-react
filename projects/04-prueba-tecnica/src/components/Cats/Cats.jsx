cc4e85b43cf86844eb77aeb176581d6a6b4c4cc9
joaquinlari <larijoaquin@gmail.com>
2024-07-24 12:37:08 -0300

    Handle errors

--- projects/04-prueba-tecnica/src/components/Cats/Cats.jsx
+++ projects/04-prueba-tecnica/src/components/Cats/Cats.jsx
@@ -11,16 +11,26 @@ const Cats = () => {
 
     const [fact, setFact] = useState()
     const [imageUrl, setImageUrl] = useState()
+    const [error, setError] = useState(null);
 
-    // para recuperar la cita al cargar la pagina
-    useEffect(() => {
+    const getRandomFact = () => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return (res.json())
+
+            })
+
             .then(data => {
                 const { fact } = data
                 setFact(fact)
             })
-    }, [])
+            .catch(error => setError(error.message))
+    }
+    // para recuperar la cita al cargar la pagina
+    useEffect(getRandomFact, [])
 
     // para recuperar la imagen cada vez que tengamos una cita nueva
     useEffect(() => {
@@ -31,17 +41,32 @@ const Cats = () => {
         console.log(threeFirstWord)
 
         fetch(`${CAT_PREFIX_IMAGE_URL}/cat/says/${threeFirstWord}?size=50&color=red`)
-            .then(res => res.blob())
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return (res.blob())
+            })
             .then(blob => {
                 const imageObjectUrl = URL.createObjectURL(blob)
                 setImageUrl(imageObjectUrl)
             })
+            .catch(error => setError(error.message))
     }, [fact])
+
+    const handleClick = () => {
+        setError(null);
+        getRandomFact()
+    }
 
     return (
         <main className="wrap">
             <div className="text">
                 <h2 className="title">App de Gatitos</h2>
+                <button onClick={handleClick} className="button">
+                    Get new fact
+                </button>
+                {error && <p className="error">{typeof error === 'string' ? error : 'An unexpected error occurred'}</p>}
                 {fact && <p className="subtitle">{fact}</p>}
             </div>
             <div className="album">
