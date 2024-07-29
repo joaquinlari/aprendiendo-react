a56139dadb4b8b934902ff797f3227be03a4d9ba
joaquinlari <larijoaquin@gmail.com>
2024-07-28 22:49:20 -0300

    Show movies

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import responseMoviesWithoutResults from './mocks/no-results.json'
 
 function App() {
+
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
 
   return (
     <>
@@ -9,12 +14,30 @@ function App() {
           <h1>Buscador de películas</h1>
           <input placeholder="Avengers, Star Wars, Attack on titan..."></input>
           <button type="submit">Buscar</button>
-
         </header>
+
         <main className="box-results">
-          Acá se muestran los resultados
+          {
+            hasMovies
+              ? (
+                < ul >
+                  {
+                    movies.map(movie => (
+                      <li className="box-results-ul_li" key={movie.imdbID}>
+                        <h3>{movie.Title}</h3>
+                        <p>{movie.Year}</p>
+                        <img src={movie.Poster} alt={movie.Title}></img>
+                      </li>
+                    ))
+                  }
+                </ul>
+              )
+              : (
+                <p>No se encontraron resultados</p>
+              )
+          }
         </main>
-      </div>
+      </div >
     </>
   )
 }
