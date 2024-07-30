b6c20b1f913b6931325445170182edc513cb2fa3
joaquinlari <larijoaquin@gmail.com>
2024-07-30 20:48:18 -0300

    Improved code

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -2,14 +2,28 @@ import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+
+
 function App() {
+
   const { movies } = useMovies()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { query } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log(query)
+  }
+
   return (
     <div className="container">
       <header className="box-header">
         <h1>Buscador de películas</h1>
-        <input placeholder="Avengers, Star Wars, Attack on titan..."></input>
-        <button type="submit">Buscar</button>
+        <form onSubmit={handleSubmit}>
+          <input name="query" placeholder="Avengers, Star Wars, Attack on titan..."></input>
+          <button type="submit">Buscar</button>
+        </form>
       </header>
 
       <main className="box-results">
