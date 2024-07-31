b55df502e165338910889a5c3b885515473a9714
joaquinlari <larijoaquin@gmail.com>
2024-07-31 19:04:29 -0300

    Add search and conditions

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,21 +1,47 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useState, useEffect } from 'react'
 
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
 
+  useEffect(() => {
+    if (search === '') {
+      setError('No se puedebuscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+  return { search, updateSearch, error }
+
+}
 
 function App() {
 
   const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const { query } = Object.fromEntries(
-      new window.FormData(event.target)
-    )
-    console.log(query)
+    console.log({ search })
   }
-
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
   return (
     <div className="container">
       <header className="box-header">
