b47cf8d2b79f1a4c6e2f9fd3fed1f8b7ff530ac8
joaquinlari <larijoaquin@gmail.com>
2024-08-03 18:08:07 -0300

    Improved code

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -5,17 +5,18 @@ import { useState } from 'react'
 export function useMovies({ search }) {
 
     const [responseMovies, setResponseMovies] = useState([])
-    const movies = responseMovies.Search
-
-    const mappedMovies = movies?.map(movie => ({
-        id: movie.imdbID,
-        title: movie.Title,
-        year: movie.Year,
-        poster: movie.Poster
-    }))
 
     const getMovies = () => {
 
+        if (search) {
+            fetch(`https://www.omdbapi.com/?apikey=fb6a907c&s=${search}`)
+                .then(res => res.json())
+                .then(json => {
+                    setResponseMovies(json)
+                })
+        } else {
+            setResponseMovies(withoutResults)
+        }
     }
 
     return { movies: mappedMovies, getMovies }
--- projects/05-react-buscador-peliculas/src/services/movies.js
+++ projects/05-react-buscador-peliculas/src/services/movies.js
@@ -1,14 +1,21 @@
 const API_KEY = 'fb6a907c'
 
 export const searchMovies = async ({ search }) => {
+    if (search === '') return null
 
-    if (search) {
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-            .then(res => res.json())
-            .then(json => {
-                setResponseMovies(json)
-            })
-    } else {
-        setResponseMovies(withoutResults)
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        const json = await response.json
+
+        const movies = json.Search
+
+        return movies?.map(movie => ({
+            id: movie.imdbID,
+            title: movie.Title,
+            year: movie.Year,
+            poster: movie.Poster
+        }))
+    } catch (e) {
+        throw new Error('Error searching movies')
     }
 }
