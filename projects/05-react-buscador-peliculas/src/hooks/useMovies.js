9460adfb6015be797410ae1ea8619e27d8f314ac
joaquinlari <larijoaquin@gmail.com>
2024-08-04 21:42:05 -0300

    prevent the same search from being done

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { searchMovies } from '../services/movies'
+
 
 export function useMovies({ search }) {
 
     const [movies, setMovies] = useState([])
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
+    const previousSearch = useRef(search)
 
     const getMovies = async () => {
+        if (search === previousSearch.current) return
         try {
             setLoading(true)
             setError(false)
+            previousSearch.current = search
             const newMovies = await searchMovies({ search })
             setMovies(newMovies)
         } catch (e) {
