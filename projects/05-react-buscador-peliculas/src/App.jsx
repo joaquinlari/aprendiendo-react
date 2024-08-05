487a177bd00fb7c195d4768aa8fc4d462d87bdf5
joaquinlari <larijoaquin@gmail.com>
2024-08-04 21:05:46 -0300

    add: loading state

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -38,7 +38,7 @@ function useSearch() {
 function App() {
 
   const { search, updateSearch, error } = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search })
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -59,7 +59,9 @@ function App() {
       </header>
 
       <main>
-        <Movies movies={movies} />
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
       </main>
     </div >
   )
