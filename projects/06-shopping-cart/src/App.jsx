ea9a4d6c0bac38fde94aa3102dcff74cb464a6e0
joaquinlari <larijoaquin@gmail.com>
2024-08-10 22:17:30 -0300

    Showing products

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,6 +1,9 @@
+import { Products } from "./components/Products"
+import { products } from "./mocks/products.json"
+
 function App() {
   return (
-    <h1>Shopping Cart 🛒</h1>
+    <Products products={products} />
   )
 }
 
