8f18fd58f37ae5085a3d00529417bdf674ba2d09
joaquinlari <larijoaquin@gmail.com>
2024-08-11 05:00:57 -0300

    Create header component

---
+++ projects/06-shopping-cart/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Filters } from './Filters.jsx'
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>React Shop</h1>
+            <Filters />
+        </header>
+    )
+}
+
+export default Header
