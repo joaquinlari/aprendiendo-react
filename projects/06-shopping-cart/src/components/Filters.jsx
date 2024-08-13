c44c7977f1c20c157c75842f524821a245de5d66
joaquinlari <larijoaquin@gmail.com>
2024-08-12 23:12:53 -0300

    Input range

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -3,6 +3,12 @@ export function Filters() {
         <section className='filters'>
             <div>
                 <label htmlFor='price'>Price</label>
+                <input
+                    type='range'
+                    id='price'
+                    min='0'
+                    max='1000'
+                />
             </div>
         </section>
     )
