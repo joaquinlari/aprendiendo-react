735b04b683e422fe09d418cb3d59aa779e4bf561
joaquinlari <larijoaquin@gmail.com>
2024-08-16 02:05:56 -0300

    Add PropTypes for filters

--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,4 +1,9 @@
 import './Footer.css'
+import PropTypes from 'prop-types';
+
+Footer.propTypes = {
+    filters: PropTypes.object.isRequired
+};
 
 export function Footer({ filters }) {
     // const { filters } = useFilters()
