1054999cf80f94f21a941d95c76dcb1267070992
joaquinlari <larijoaquin@gmail.com>
2024-08-17 23:28:48 -0300

    Change StrictMode -> FiltersProvider

--- projects/06-shopping-cart/src/main.jsx
+++ projects/06-shopping-cart/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { FiltersProvider } from './context/filters.js'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FiltersProvider>
     <App />
-  </StrictMode>,
+  </FiltersProvider>,
 )
