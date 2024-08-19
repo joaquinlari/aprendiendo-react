438ddffc4bada7cccfbfafbb27366a088e445346
joaquinlari <larijoaquin@gmail.com>
2024-08-18 21:20:23 -0300

    Using useContext

--- projects/06-shopping-cart/src/context/filters.jsx
+++ projects/06-shopping-cart/src/context/filters.jsx
@@ -1,14 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // 1. Crear el contexto
 export const FiltersContext = createContext()
 
 //2. Crear el Provider
 export function FiltersProvider({ children }) {
+    const [filters, setFilters] = useState({
+        category: 'all',
+        minPrice: 0
+    })
     return (
         <FiltersContext.Provider value={{
-            category: 'all',
-            minPrice: 0
+            filters,
+            setFilters
         }}>
             {children}
         </FiltersContext.Provider>
--- projects/06-shopping-cart/src/main.jsx
+++ projects/06-shopping-cart/src/main.jsx
@@ -1,4 +1,4 @@
-import { FiltersProvider } from './context/filters.js'
+import { FiltersProvider } from './context/filters.jsx'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
