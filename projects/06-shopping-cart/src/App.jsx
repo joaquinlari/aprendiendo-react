f4515c03b28475efc446b4d6d2c659b999a6205a
joaquinlari <larijoaquin@gmail.com>
2024-08-19 05:03:20 -0300

    Clean code

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,15 +1,13 @@
 import { Products } from "./components/Products"
 import { products as initialProducts } from "./mocks/products.json"
-import { useState } from 'react'
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
 import { useFilters } from "./hooks/useFilters"
 
 function App() {
-  const [products] = useState(initialProducts)
   const { filters, filterProducts } = useFilters()
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(initialProducts)
 
   return (
     <>
