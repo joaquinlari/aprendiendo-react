7f1865bd0e99e34c25528ce654fb271937e5f4cb
joaquinlari <larijoaquin@gmail.com>
2024-08-19 05:04:47 -0300

    Clean footer

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -6,14 +6,14 @@ import { IS_DEVELOPMENT } from "./config"
 import { useFilters } from "./hooks/useFilters"
 
 function App() {
-  const { filters, filterProducts } = useFilters()
+  const { filterProducts } = useFilters()
   const filteredProducts = filterProducts(initialProducts)
 
   return (
     <>
       <Header />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer filters={filters} />}
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,7 +1,8 @@
+import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 
-export function Footer({ filters }) {
-    // const { filters } = useFilters()
+export function Footer() {
+    const { filters } = useFilters
 
     return (
         <footer className='footer'>
