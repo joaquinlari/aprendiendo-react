1e515b217e57219515ee330482a528deabcda6d8
joaquinlari <larijoaquin@gmail.com>
2024-08-18 23:29:05 -0300

    customHook

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,25 +1,10 @@
 import { Products } from "./components/Products"
 import { products as initialProducts } from "./mocks/products.json"
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
-import { FiltersContext } from "./context/filters"
-
-function useFilters() {
-
-  const { filters, setFilters } = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === 'all' || product.category === filters.category)
-      )
-    })
-  }
-  return { filters, filterProducts, setFilters }
-}
+import { useFilters } from "./hooks/useFilters"
 
 function App() {
   const [products] = useState(initialProducts)
---
+++ projects/06-shopping-cart/src/hooks/useFilters.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import { FiltersContext } from "./context/filters"
+
+export function useFilters() {
+
+    const { filters, setFilters } = useContext(FiltersContext)
+
+    const filterProducts = (products) => {
+        return products.filter(product => {
+            return (
+                product.price >= filters.minPrice &&
+                (filters.category === 'all' || product.category === filters.category)
+            )
+        })
+    }
+    return { filters, filterProducts, setFilters }
+}
