61031d4c98de7322eb3d6b5f64452bf0bce770f1
joaquinlari <larijoaquin@gmail.com>
2024-08-21 04:18:50 -0300

    Add remove from cart

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -5,18 +5,19 @@ import { Footer } from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
 import { useFilters } from "./hooks/useFilters"
 import { Cart } from "./components/Cart"
+import { CartProvider } from "./context/cart"
 
 function App() {
   const { filterProducts } = useFilters()
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartProvider>
       <Header />
       <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
-    </>
+    </CartProvider>
   )
 }
 
--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import { useFilters } from '../hooks/useFilters'
+import { useCart } from '../hooks/useCart'
 import './Footer.css'
 
 export function Footer() {
     const { filters } = useFilters()
+    const { cart } = useCart()
 
     return (
         <footer className='footer'>
-            {JSON.stringify(filters, null, 2)}
+            {
+                JSON.stringify(filters, null, 2)
+            }
+            {
+                JSON.stringify(cart, null, 2)
+            }
         </footer>
     )
 } 
