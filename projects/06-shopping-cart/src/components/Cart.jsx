add459730cecb7532d5c78835f343aedefc82c19
joaquinlari <larijoaquin@gmail.com>
2024-08-20 21:54:35 -0300

    Input checkbox

--- projects/06-shopping-cart/src/components/Cart.jsx
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -5,9 +5,14 @@ export function Cart() {
     const cartCheckboxId = useId();
     return (
         <>
-            <label className="cart-button" htmlFor='cart'>
+            <label className="cart-button" htmlFor={cartCheckboxId}>
                 <CartIcon />
             </label>
+            <input id={cartCheckboxId} type='checkbox' hidden />
+
+            <aside>
+
+            </aside>
         </>
     )
 }
