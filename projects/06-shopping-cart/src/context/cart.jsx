69a1a065f7cc68f88c8f6e6737fc96ffe3a21b46
joaquinlari <larijoaquin@gmail.com>
2024-08-21 02:58:54 -0300

    Add function remove from cart

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -23,6 +23,10 @@ export function CartProvider({ children }) {
         ]))
     }
 
+    const removeFromCart = product => {
+        setCart(prevState => prevState.filter(item => item.id !== product.id))
+    }
+
     const clearCart = () => {
         setCart([])
     }
@@ -30,6 +34,7 @@ export function CartProvider({ children }) {
         <CartContext.Provider value={{
             cart,
             addToCart,
+            removeFromCart,
             clearCart
         }}>
             {children}
