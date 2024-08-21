791567cee5ab25633ee3b696a64cb21e526ded37
joaquinlari <larijoaquin@gmail.com>
2024-08-20 22:33:00 -0300

    Import cart in App

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
 import { useFilters } from "./hooks/useFilters"
+import { Cart } from "./components/Cart"
 
 function App() {
   const { filterProducts } = useFilters()
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
     </>
