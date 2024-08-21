21e04153c27e1b5a3b7e7a5638bfaea25a082ad1
joaquinlari <larijoaquin@gmail.com>
2024-08-21 02:56:15 -0300

    Add function addToCart

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -6,6 +6,21 @@ export function CartProvider({ children }) {
     const [cart, setCart] = useState([])
 
     const addToCart = product => {
+        const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+        if (productInCartIndex >= 0) {
+            const newCart = structuredClone(cart)
+            newCart[productInCartIndex].quantity += 1
+            return setCart(newCart)
+        }
+
+        setCart(prevState => ([
+            ...prevState,
+            {
+                ...product,
+                quantity: 1
+            }
+        ]))
     }
 
     const clearCart = () => {
