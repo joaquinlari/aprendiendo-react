31f7d818ecec091154546e1ad7e6701554d3eeb3
joaquinlari <larijoaquin@gmail.com>
2024-08-23 02:26:56 -0300

    Changing useState to useReducer

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -13,46 +13,50 @@ export function CartProvider({ children }) {
             case 'ADD_TO_CART': {
                 const { id } = actionPayload
                 const productInCartIndex = state.findIndex(item => item.id === id)
-            }
-            case 'REMOVE_CART':
-            case 'CLEAR_CART':
 
+                if (productInCartIndex >= 0) {
+                    const newState = structuredClone(state)
+                    newState[productInCartIndex].quantity += 1
+                    return newState
+                }
+
+                return [
+                    ...state,
+                    {
+                        ...actionPayload, //product
+                        quantity: 1
+                    }
+                ]
+            }
+
+            case 'REMOVE_FROM_CART': {
+                const { id } = actionPayload
+                return state.filter(item => item.id !== id)
+            }
+            case 'CLEAR_CART': {
+                return initialState
+            }
         }
+
     }
-    /* const addToCart = product => {
-        const productInCartIndex = cart.findIndex(item => item.id === product.id)
+}
 
-        if (productInCartIndex >= 0) {
-            const newCart = structuredClone(cart)
-            newCart[productInCartIndex].quantity += 1
-            return setCart(newCart)
-        }
-
-        setCart(prevState => ([
-            ...prevState,
-            {
-                ...product,
-                quantity: 1
-            }
-        ]))
-    } */
-
-    /*  const removeFromCart = product => {
-         setCart(prevState => prevState.filter(item => item.id !== product.id))
-     }
+/*  const removeFromCart = product => {
+     setCart(prevState => prevState.filter(item => item.id !== product.id))
+ }
  
-     const clearCart = () => {
-         setCart([])
-     } */
-    return (
-        <CartContext.Provider value={{
-            state,
-            addToCart,
-            removeFromCart,
-            clearCart
-        }}>
-            {children}
-        </CartContext.Provider>
-    )
+ const clearCart = () => {
+     setCart([])
+ } */
+return (
+    <CartContext.Provider value={{
+        state,
+        addToCart,
+        removeFromCart,
+        clearCart
+    }}>
+        {children}
+    </CartContext.Provider>
+)
 
 }
