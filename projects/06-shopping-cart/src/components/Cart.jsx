259b21df2ef6db5d885ea766bd2d0e9b62238c26
joaquinlari <larijoaquin@gmail.com>
2024-08-22 21:40:06 -0300

    Fix show products in cart

--- projects/06-shopping-cart/src/components/Cart.jsx
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -7,20 +7,22 @@ export function Cart() {
     const cartCheckboxId = useId();
 
     function CartItem({ thumbnail, title, price, quantity, addToCart }) {
-        <li>
-            <img src={thumbnail}
-                alt={title}
-            />
-            <div>
-                <strong>{title}</strong> - ${price}
-            </div>
-            <footer>
-                <small onClick={addToCart}>
-                    Qty: {quantity}
-                </small>
-                <button>+</button>
-            </footer>
-        </li>
+        return (
+            <li>
+                <img src={thumbnail}
+                    alt={title}
+                />
+                <div>
+                    <strong>{title}</strong> - ${price}
+                </div>
+                <footer>
+                    <small onClick={addToCart}>
+                        Qty: {quantity}
+                    </small>
+                    <button>+</button>
+                </footer>
+            </li>
+        )
     }
 
     const { cart, clearCart, addToCart } = useCart()
