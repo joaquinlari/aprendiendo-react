38afe8912a6d38f4281b7f775e3e1e87f49162fe
joaquinlari <larijoaquin@gmail.com>
2024-08-24 22:51:01 -0300

    Change estilnt's rules

--- projects/07-todo-app-ts/eslint.config.js
+++ projects/07-todo-app-ts/eslint.config.js
@@ -5,9 +5,14 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-unused-vars": ["error", { "varsIgnorePattern": "^React$" }]
+    }
+  }
 ];
