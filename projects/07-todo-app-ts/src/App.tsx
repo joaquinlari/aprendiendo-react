bedd494c6ea29194ffc86b5c349d522d52f6f1e4
joaquinlari <larijoaquin@gmail.com>
2024-08-24 22:48:53 -0300

    Cleaning code

--- projects/07-todo-app-ts/src/App.tsx
+++ projects/07-todo-app-ts/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
