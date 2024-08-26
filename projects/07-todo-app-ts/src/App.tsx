80aabbbb111cb8a6cdbdb1c96e55048a7a72ae2d
joaquinlari <larijoaquin@gmail.com>
2024-08-26 04:21:25 -0300

    Show todos

--- projects/07-todo-app-ts/src/App.tsx
+++ projects/07-todo-app-ts/src/App.tsx
@@ -1,7 +1,35 @@
-const App = () => {
+import { useState } from "react";
+
+const mocksTodos = [
+  {
+    id: "1",
+    title: "todo 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "todo 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "todo 3",
+    completed: false,
+  },
+];
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mocksTodos);
   return (
     <>
       <h1>To-do App</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} data-state={todo.completed.toString()}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
