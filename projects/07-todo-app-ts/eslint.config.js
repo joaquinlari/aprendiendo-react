8aca5ea6d1e9b2cd7ad49f5e1ab0e15ab8e76d11
joaquinlari <larijoaquin@gmail.com>
2024-08-25 22:42:49 -0300

    Update eslint rules

--- projects/07-todo-app-ts/eslint.config.js
+++ projects/07-todo-app-ts/eslint.config.js
@@ -12,7 +12,6 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "no-unused-vars": ["error", { "varsIgnorePattern": "^React$" }],
       "react/react-in-jsx-scope": "off"
     }
   }
