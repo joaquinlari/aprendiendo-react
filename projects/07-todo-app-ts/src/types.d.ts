c4aa6f8c23be77c26510f879515bfc96fa9b9697
joaquinlari <larijoaquin@gmail.com>
2024-08-29 23:08:27 -0300

    add type completed

--- projects/07-todo-app-ts/src/types.d.ts
+++ projects/07-todo-app-ts/src/types.d.ts
@@ -6,5 +6,6 @@ export interface Todo {
 
 export type TodoId = Pick<Todo, "id">;
 export type TodoTitle = Pick<Todo, "title">;
+export type TodoCompleted = Pick<Todo, "completed">;
 
 export type ListOfTodos = Todo[];
