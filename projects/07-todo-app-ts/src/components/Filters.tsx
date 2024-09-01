56d1ec99124c9f48f4c79679316b8cbdb71b0109
joaquinlari <larijoaquin@gmail.com>
2024-08-31 23:31:49 -0300

    export filters_button

--- projects/07-todo-app-ts/src/components/Filters.tsx
+++ projects/07-todo-app-ts/src/components/Filters.tsx
@@ -1,3 +1,5 @@
+import { TODO_FILTERS } from "../consts";
+
 export const Filters: React.FC<Props> = ({
   filterSelected,
   onFilterChange,
--- projects/07-todo-app-ts/src/consts.ts
+++ projects/07-todo-app-ts/src/consts.ts
@@ -3,3 +3,18 @@ export const TODO_FILTERS = {
   ACTIVE: "active",
   COMPLETED: "completed",
 } as const; //de lectura
+
+export const FILTERS_BUTTONS = {
+  [TODO_FILTERS.ALL]: {
+    literal: "Todos",
+    href: `/?filter=${TODO_FILTERS.ALL}`,
+  },
+  [TODO_FILTERS.ACTIVE]: {
+    literal: "Activo",
+    href: `/?filter=${TODO_FILTERS.ACTIVE}`,
+  },
+  [TODO_FILTERS.COMPLETED]: {
+    literal: "Completado",
+    href: `/?filter=${TODO_FILTERS.COMPLETED}`,
+  },
+};
