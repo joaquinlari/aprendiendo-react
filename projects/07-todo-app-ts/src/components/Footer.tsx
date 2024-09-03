a34f3ca4dc07317d72d34f138d01144343585c3c
joaquinlari <larijoaquin@gmail.com>
2024-09-02 22:23:46 -0300

    Types interface Props

--- projects/07-todo-app-ts/src/components/Footer.tsx
+++ projects/07-todo-app-ts/src/components/Footer.tsx
@@ -1,14 +1,23 @@
+import { FilterValue } from "../types";
 import { Filters } from "./Filters";
 
+interface Props {
+  activeCount: number;
+  completedCount: 0;
+  filterSelected: FilterValue;
+  onClearCompleted: () => void;
+}
+
 export const Footer: React.FC<Props> = ({
-  activeCount,
-  todos,
+  activeCount = 0,
+  completedCount = 0,
+  filterSelected,
   onClearCompleted,
 }) => {
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>{todos.length}</strong> tareas pendientes
+        <strong>{activeCount}</strong> tareas pendientes
       </span>
 
       <Filters filterSelected={} onFilterChange={() => {}} />
