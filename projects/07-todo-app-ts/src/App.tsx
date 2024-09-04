70cf231c4e44204812931bb080af7da53c942822
joaquinlari <larijoaquin@gmail.com>
2024-09-03 22:26:45 -0300

    Function to handle delete completed

--- projects/07-todo-app-ts/src/App.tsx
+++ projects/07-todo-app-ts/src/App.tsx
@@ -53,6 +53,11 @@ const App = (): JSX.Element => {
     setFilterSelected(filter);
   };
 
+  const handleRemoveAllCompleted = (): void => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
   const activeCount = todos.filter((todo) => !todo.completed).length;
   const completedCount = todos.length - activeCount;
 
@@ -74,7 +79,7 @@ const App = (): JSX.Element => {
         activeCount={activeCount}
         completedCount={completedCount}
         filterSelected={filterSelected}
-        onClearCompleted={() => {}}
+        onClearCompleted={handleRemoveAllCompleted}
         handleFiltersChange={handleFilterChange}
       />
     </div>
