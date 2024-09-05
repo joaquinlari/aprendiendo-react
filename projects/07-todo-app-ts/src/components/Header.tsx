2f3bc923d6a421bec8f897b97863d08ced39edb3
joaquinlari <larijoaquin@gmail.com>
2024-09-04 23:29:04 -0300

    import createtodo Component

--- projects/07-todo-app-ts/src/components/Header.tsx
+++ projects/07-todo-app-ts/src/components/Header.tsx
@@ -1,5 +1,5 @@
 interface Props {
-  onAddTodo: (title: string) => void;
+  onAddTodo: ({ title }: TodoTitle) => void;
 }
 
 export const Header: React.FC<Props> = ({ onAddTodo }) => {
@@ -12,6 +12,8 @@ export const Header: React.FC<Props> = ({ onAddTodo }) => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png"
         />
       </h1>
+
+      <CreateTodo saveTodo={onAddTodo}
     </header>
   );
 };
