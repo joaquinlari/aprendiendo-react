6cb7562952280f426a7cc0ff387e7de5e58cc985
joaquinlari <larijoaquin@gmail.com>
2024-09-06 22:32:19 -0300

    Add autoAnimate

--- projects/07-todo-app-ts/src/components/Todos.tsx
+++ projects/07-todo-app-ts/src/components/Todos.tsx
@@ -1,15 +1,16 @@
 import { TodoId, type ListOfTodos, type TodoType } from "../types";
 import { Todo } from "./Todo";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 interface Props {
   todos: ListOfTodos;
   onCompleted: ({ id, completed }: Pick<TodoType, "id" | "completed">) => void;
   onRemove: (id: TodoId) => void;
 }
-
+const [parent] = useAutoAnimate();
 export const Todos: React.FC<Props> = ({ todos, onRemove, onCompleted }) => {
   return (
-    <ul className="todo-list">
+    <ul className="todo-list" ref={parent}>
       {todos.map((todo) => (
         <li key={todo.id} className={`${todo.completed ? "completed" : ""}`}>
           <Todo
